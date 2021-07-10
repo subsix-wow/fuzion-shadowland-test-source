ad065c4e1c1cfe6b3ea4529115950ca84cdf97ca
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-07-10 16:05:57 +0300

    update

--- src/server/scripts/Pandaria/ThroneOfThunder/throne_of_thunder.h
+++ src/server/scripts/Pandaria/ThroneOfThunder/throne_of_thunder.h
@@ -1,5 +1,5 @@
 /*
- * Trinity Core and update by MoPCore Forums
+ * Copyright 2021 FuzionCore
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
